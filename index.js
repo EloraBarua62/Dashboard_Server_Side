ac18836e4794293f7b37dabbe2b2459f5608a8f0
EloraBarua <elorabarua62@gmail.com>
2022-10-30 14:37:22 +0600

    get and delete method implemented

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
+
+
+// Database connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ohx16.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const productCollection = client.db("dashboard").collection("product");
+        app.get('/all_product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const result = await cursor.toArray()
+            res.send(result);
+        })
+
+        app.delete('/product_delete/:id' , async(req,res)=> {
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)};
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        })
+        
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
+
 
 // end_point
 app.get('/',(req,res) => {
