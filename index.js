8441237977306b15dc5800a4ad5163d9d7a01e61
EloraBarua <elorabarua62@gmail.com>
2022-10-30 18:18:08 +0600

    create and delete done

--- index.js
+++ index.js
@@ -17,17 +17,64 @@ async function run(){
     try{
         await client.connect();
         const productCollection = client.db("dashboard").collection("product");
+        const userCollection = client.db("dashboard").collection("user");
         app.get('/all_product', async (req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
             const result = await cursor.toArray()
             res.send(result);
         })
+        app.get('/all_user', async (req, res) => {
+            const query = {};
+            const cursor = userCollection.find(query);
+            const result = await cursor.toArray()
+            res.send(result);
+        })
 
+        
+        
+        // POST
+        app.post('/add_product' , async(req,res) => {
+            const product = req.body;
+            const addProduct = await productCollection.insertOne(product);
+            res.send(addProduct);
+        })
+        // POST
+        app.post('/add_user' , async(req,res) => {
+            const user = req.body;
+            const addUser = await userCollection.insertOne(user);
+            res.send(addUser);
+        })
+
+
+        app.put('/update_product/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateQuantity = req.body;
+            const query = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateBookInfo = {
+                $set: {
+                    quantity: updateQuantity.quantity
+                }
+            };
+            const book = await booksCollection.updateOne(query, updateBookInfo, options);
+            res.send(book);
+        });
+
+
+        // DELETE
         app.delete('/product_delete/:id' , async(req,res)=> {
             const id = req.params.id;
             const query = {_id:ObjectId(id)};
             const result = await productCollection.deleteOne(query);
+            res.send(result);
+        })
+        
+        // DELETE
+        app.delete('/user_delete/:id' , async(req,res)=> {
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)};
+            const result = await userCollection.deleteOne(query);
             res.send(result);
         })
         
